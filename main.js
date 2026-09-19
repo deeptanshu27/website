587a4ab97e064db35a468130ace112ac369c56d7
deeptanshu27 <deeptanshu1207@gmail.com>
2026-09-20 01:57:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
+/*
+issues: too much text on small screens
+change selected text to ######## :D
+*/
+
+
 function init() {
-    if (mobileAndTabletCheck()) {
-        if (localStorage.getItem("drawPrev") == "true") {
-            loadBackground();
-        } else {
-            alert("woah")
-            checkMotionPermission();
-        }
-    } else {
-        loadBackground();
-    }
+    loadBackground();
 }
-
-window.mobileAndTabletCheck = function() {
-  let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
-};
-
-
 function loadBackground() {
-    let width = document.body.clientWidth;
-    let height = document.body.clientHeight;
-    let i = 0;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let dimsRatio = (width*height)/(1920*1200);
+    let drawCount = 100*dimsRatio;
+    if (drawCount < 50) drawCount = 50;
+    console.log(drawCount);
     if (localStorage.getItem("drawPrev") == "true") {
-        for (i = 0; i < 100; i++) {
+        for (let i = 0; i < drawCount; i++) {
             drawText(width, height);
         }
     } else {
@@ -36,9 +28,13 @@ function loadBackground() {
     }
 }
 
+let pChoices = ["silver", "silver", "silver", "silver",
+    "#####", "#####",
+    "a sphinx",
+    "[redacted]"];
 function drawText(width, height) {
     let p = document.createElement("p");
-    p.textContent = "silver";
+    p.textContent = pChoices[Math.floor(Math.random() * pChoices.length)];
     p.style.position = "absolute";
     p.style.bottom = (Math.random() * height) + "px";
     p.style.left = (Math.random() * width) + "px";
@@ -48,54 +44,4 @@ function drawText(width, height) {
     p.style.margin = 0;
     p.style.zIndex = "-1";
     document.body.append(p);
-}
-
-async function checkMotionPermission() {
-
-    // Any browser using requestPermission API
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-
-        // If previously granted, user will see no prompts and listeners get setup right away.
-        // If error, we show special UI to the user.
-        // FYI, "requestPermission" acts more like "check permission" on the device.
-        await DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-            if (permissionState == 'granted') {
-                // Hide special UI; no longer needed
-                btn_reqPermission.style.display = "none"
-                setMotionListeners()
-            }
-        })
-        .catch( (error) => {
-            console.log("Error getting sensor permission: %O", error)
-            // Show special UI to user, suggesting they should allow motion sensors. The tap-or-click on the button will invoke the permission dialog.
-            btn_reqPermission.style.display = "block"
-        })
-
-    // All other browsers
-    } else {
-        setMotionListeners()
-    }
-
-}
-
-async function setMotionListeners() {
-    // MOTION LISTENER
-    await window.addEventListener('devicemotion', motionListener)
-}
-
-let i = 0;
-function motionListener(event) {
-    i++;
-    if (i < 10) return;
-    console.log('Device motion event: %O', event)
-
-    // SHAKE EVENT
-    // Using rotationRate, which essentially is velocity,
-    // we check each axis (alpha, beta, gamma) whether they cross a threshold (e.g. 256).
-    // Lower = more sensitive, higher = less sensitive. 256 works nice, imho.
-    if ((event.rotationRate.alpha > 512 || event.rotationRate.beta > 512 || event.rotationRate.gamma > 512)) {
-        loadBackground();
-        window.removeEventListener('devicemotion', motionListener);
-    }
 }
